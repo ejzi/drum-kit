77f28f2d40eaf6f7a5410a302bf111b13c3be97d
ejzi <ann.zarzycka@gmail.com>
2018-07-07 21:25:58 +0200

    changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+window.addEventListener('keydown', function(e){
+    console.log(e.keyCode);
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+    console.log(audio);
 
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
+    console.log(key);
+
+    if(!audio) return;
+
+    audio.currentTime = 0;
+
+    audio.play();
+
+    key.classList.add('playing');
+    key.classList.remove('playing');
+    key.classList.toggle('playing');
+})
 
 });
